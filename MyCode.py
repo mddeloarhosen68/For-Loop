e25dd111b380321f74e0faafba3b6ad2eecd3433
mddeloarhosen68 <62032396+mddeloarhosen68@users.noreply.github.com>
2020-05-06 02:21:25 +0600

    Add files via upload

---
+++ MyCode.py
@@ -0,0 +1,36 @@
+#Ex:1
+x = ['Anon', 70, 7.2]
+
+for i in x:
+    print(i)
+
+
+#Ex:2
+
+x= 'ANON'
+
+for i in x:
+    print(i)
+
+
+#Ex:3
+
+for i in range(10):
+    print(i)
+
+
+#Ex:4
+
+for i in range(11,21,2):
+    print(i)
+
+#Ex:4
+
+for i in range(20,9,-1):
+    print(i)
+
+#Ex:5
+for i in range(1,21):
+    if i % 5 != 0:
+        print(i)
+
